6f1dad608b2d761c397b20a647aaa68435423425
Alen Ichshanov <alen.ichshanov@gmail.com>
2025-11-01 15:49:04 +0500

    step5 opt

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,23 +1,3 @@
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
-
   const Header = (props) => {
     return (
       <h1>{props.title}</h1>
@@ -54,6 +34,26 @@ const App = () => {
       <p>Number of exercises {getTotal()}</p>
     )
   }
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
 
   return (
     <div>
