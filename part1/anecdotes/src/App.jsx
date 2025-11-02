413c36cbfd1b2f3e80bbd9b0d70ac6e791b2d6e5
Alen Ichshanov <alen.ichshanov@gmail.com>
2025-11-02 17:00:58 +0500

    anecdotes: finished step 2

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -14,14 +14,24 @@ const App = () => {
    
   const [selected, setSelected] = useState(0)
 
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
   const selectRandomAnecdote = () => {
     const rndAnecdote = Math.floor(Math.random() * anecdotes.length);
     setSelected(rndAnecdote)
   }
 
+  const voteForAnecdote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
+
   return (
     <div>
       <p>{anecdotes[selected]} </p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={voteForAnecdote}>vote</button>
       <button onClick={selectRandomAnecdote}>next anecdote</button>
     </div>
   )
