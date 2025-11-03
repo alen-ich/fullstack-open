a5ccbf0a0654019205cce9220e1a1f48075f46e1
Alen Ichshanov <alen.ichshanov@gmail.com>
2025-11-03 21:41:10 +0500

    part2: finished phonebook step4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,14 +3,27 @@ import Contact from './components/Contact'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '090-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterName, setFilterName] = useState('')
+
+  const personsToShow = filterName.length
+    ? persons.filter(person => person.name.toLowerCase().includes(filterName.toLowerCase()))
+    : persons
 
   const handleNameChange = event => setNewName(event.target.value)
 
   const handleNumberChange = event => setNewNumber(event.target.value)
+
+  const handleFilterNameChange = event => {
+    setFilterName(event.target.value)
+    
+  }
 
   const addContact = event => {
     event.preventDefault()
@@ -31,15 +44,17 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addContact}>
-        <div>name: <input value={newName} onChange={handleNameChange} /></div>
-        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
-        <div><button type="submit">add</button></div>
-      </form>
+      <h1>Phonebook</h1>
+      <div>filter contacts:<input value={filterName} onChange={handleFilterNameChange} /></div>
+      <h2>Add new contact</h2>
+        <form onSubmit={addContact}>
+          <div>name: <input value={newName} onChange={handleNameChange} /></div>
+          <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+          <div><button type="submit">add</button></div>
+        </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <Contact key={person.name} contact={person} />)}
+        {personsToShow.map(person => <Contact key={person.id} contact={person} />)}
       </div>
     </div>
   )
