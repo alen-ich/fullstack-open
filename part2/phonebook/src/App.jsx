21c17f60e1d8a7ccb8751a24aac9d63625078eba
Alen Ichshanov <alen.ichshanov@gmail.com>
2025-11-04 18:26:35 +0500

    part2: finished phonebook step12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
   const [message, setMessage] = useState(null)
+  const [errMessage, setErrMessage] = useState(null)
 
   const personsToShow = filterName.length
     ? persons.filter(person => person.name.toLowerCase().includes(filterName.toLowerCase()))
@@ -35,9 +36,7 @@ const App = () => {
           .then(response => {
             setPersons(persons.map(person => person.id === updContact.id ? response : person))
             setMessage(`Updated ${response.name}`)
-            setTimeout(() => {
-              setMessage(null)
-            }, 5000)
+            setTimeout(() => setMessage(null), 5000)
           })
       }
     }
@@ -50,9 +49,7 @@ const App = () => {
         .then(response => {
           setPersons(persons.concat(response))
             setMessage(`Added ${response.name}`)
-            setTimeout(() => {
-              setMessage(null)
-            }, 5000)
+            setTimeout(() => setMessage(null), 5000)
         })
     }
     setNewName('')
@@ -63,6 +60,10 @@ const App = () => {
     if(window.confirm(`Delete ${contact.name} ?`)) {
     contactsService.deleteContact(contact.id)
       .then(response => setPersons(persons.filter(person => person.id !== response.id)))
+      .catch(() => {
+        setErrMessage(`Informaion of ${contact.name} has already been removed from server`)
+        setTimeout(() => setErrMessage(null), 5000)
+      })
     }
   }
 
@@ -74,7 +75,7 @@ const App = () => {
   return (
     <div>
       <h1>Phonebook</h1>
-      <Notification message={message} />
+      <Notification message={message} errMessage={errMessage} />
       <Filter filterName={filterName} onFilterChange={handleFilterNameChange} />
       <NewContactForm 
         newName={newName} 
--- part2/phonebook/src/components/Notification.jsx
+++ part2/phonebook/src/components/Notification.jsx
@@ -1,12 +1,14 @@
-const Notification = ({ message }) => {
-  if (message === null) {
+const Notification = ({ message, errMessage }) => {
+  if (message === null && errMessage === null) {
     return null
   }
-
+  else if (errMessage){
+    return (
+        <div className='error'>{errMessage}</div>
+    )
+  }
   return (
-    <div className='message'>
-      {message}
-    </div>
+    <div className='message'>{message}</div>
   )
 }
 
